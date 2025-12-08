9b7c5b96eb3c6f43d86d6748ba21ba97ecc295c7
hiuhiu0902 <Dotrunghieund9.9@gmail.com>
2025-12-08 19:34:11 +0700

    fix

--- src/main/java/com/demo/sell_card_demo1/config/S3Config.java
+++ src/main/java/com/demo/sell_card_demo1/config/S3Config.java
@@ -3,17 +3,13 @@ package com.demo.sell_card_demo1.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
-    @Value("${aws.s3.access-key}")
-    private String accessKey;
-    @Value("${aws.s3.secret-key}")
-    private String secretKey;
+
     @Value("${aws.s3.region}")
     private String region;
 
@@ -21,10 +17,10 @@ public class S3Config {
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKey, secretKey)
-                ))
+                // Dòng này rất quan trọng: Nó cho phép code tự tìm quyền
+                // 1. Nếu chạy Local: Tìm trong file ~/.aws/credentials
+                // 2. Nếu chạy EC2: Tìm IAM Role đã gắn vào EC2 (bảo mật tuyệt đối)
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
-
 }
